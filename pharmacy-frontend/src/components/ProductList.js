df65b0126786939ee3263b1e7ebb6575f44c0a45
curtis488 <csnomensah@st.ug.edu.gh>
2024-07-27 11:50:44 +0000

    .

--- pharmacy-frontend/src/components/ProductList.js
+++ pharmacy-frontend/src/components/ProductList.js
@@ -51,8 +51,8 @@ const ProductList = () => {
         <h1>Products</h1>
         <ul className="product-list">
           {products.map(product => (
-            <li key={product.id || 'default-id'} className="product-item">
-              <Link to={`/products/${product.id}`} className="product-link">
+            <li key={product._id || 'default-id'} className="product-item">
+              <Link to={`/products/${product._id}`} className="product-link">
                 <img src={`http://localhost:3000/images/${product.image}`} alt={product.title} className="product-image" />
                 <div className="product-info">
                   <h2 className="product-title">{product.name}</h2>
